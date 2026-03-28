27849b9f93ce8ca72a8ac539c356873ca0bd6faa
Atharsh Krishnamoorthy <atharshkrishnamoorthy@gmail.com>
2026-03-28 13:26:49 +0530

    config changes

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreDuringBuilds: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
